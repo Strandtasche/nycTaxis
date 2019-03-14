d4d5f148c1d442314db869f48d75922993eda335
Tobias Hornberger <saibot1207@googlemail.com>
2019-03-14 23:56:23 +0100

    implement basic loading functionality

--- nycTaxis/core.py
+++ nycTaxis/core.py
@@ -2,5 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import os
+import glob
 
 
+def loadData(path):
+	# load a single csv file
+	dataFrameTaxisFull = pd.read_csv(path)
+
+	# determine duration of individual trips
+	dataFrameTaxisFull['tpep_pickup_datetime'] = pd.to_datetime(dataFrameTaxisFull['tpep_pickup_datetime'])
+	dataFrameTaxisFull['tpep_dropoff_datetime'] = pd.to_datetime(dataFrameTaxisFull['tpep_dropoff_datetime'])
+
+	dataFrameDuration = dataFrameTaxisFull['tpep_dropoff_datetime'] - dataFrameTaxisFull['tpep_pickup_datetime']
+
+	return dataFrameDuration
+
+
+def main():
+	dirName = os.path.dirname(__file__)
+	dataFolderPath = os.path.join(dirName, '../data/')
+
+	fileList = sorted(glob.glob(dataFolderPath + '/*.csv'))
+
+	durationList = []
+	for file in fileList:
+		durationList.append(loadData(file))
+
+	print("done loading: len(durationList) = {}".format(len(durationList)))
+
+if __name__ == '__main__':
+    main()
