3642e202bb5970ef63d37fdf03b5844907cda90a
Tobias Hornberger <saibot1207@googlemail.com>
2019-03-25 16:34:17 +0100

    naive script added

---
+++ nycTaxis/bin/getMonthlyAverageNaive.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from nycTaxis import core
+
+
+assert len(sys.argv) == 2
+dataPath = sys.argv[1]
+
+
+if os.path.isfile(dataPath):
+	averageMonth = core.averageMonthNaive(dataPath)
+	print("Average: {}".format(averageMonth))
+else:
+	print("File not found.")
