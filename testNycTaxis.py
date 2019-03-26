19ebbf7ca5d38218752b71bb8697f3b87ac6824a
Tobias Hornberger <saibot1207@googlemail.com>
2019-03-26 15:57:10 +0100

    added test cases

--- testNycTaxis.py
+++ testNycTaxis.py
@@ -23,13 +23,30 @@ def test_AverageMonthNaive():
 	average = core.averageMonthNaive('/home/tobi/Projects/BlueYonder/testData/testData01.csv')
 	assert average == 60
 
+def test_validateBad():
+
+	teststringsBad = ["2017-01-04", "notADate", "2019-99-99"]
+	testStringGood = ['2018-01-01']
+
+	testDataFrame = dataHandler.loadFolder('/home/tobi/Projects/BlueYonder/testData')
+	testDataFrame, _ = dataHandler.calculateRollingAverage(testDataFrame)
+
+	with pytest.raises(ValueError) as v:
+		core.rollingAverageDate(testDataFrame, teststringsBad[0])
+	assert 'not part of dataset' in str(v.value)
+
+	with pytest.raises(ValueError) as v:
+		core.rollingAverageDate(testDataFrame, teststringsBad[1])
+	assert 'Incorrect Date Format' in str(v.value)
+
+
+	with pytest.raises(ValueError) as v:
+		core.rollingAverageDate(testDataFrame, teststringsBad[2])
+	assert 'Incorrect Date Format' in str(v.value)
 
 
 
-
-
-
-	#Test, der überprüft, dass der Average richtig berechnet wird
+#Test, der überprüft, dass der Average richtig berechnet wird
 
 	#Test, der überprüft, dass die Daten richtig geladen werden
 
